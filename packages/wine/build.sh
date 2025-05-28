c958ef0d597861182476e5daa606f5d5049e4c8b
WINDROID-EMU <45230455+WINDROID-EMU@users.noreply.github.com>
2025-05-28 08:46:54 -0300

    build.sh

--- packages/wine/build.sh
+++ packages/wine/build.sh
@@ -1,48 +1,54 @@
-PKG_VER="10.1-10-esync-xinput"
+PKG_VER="10.8"
 PKG_CATEGORY="Wine"
 PKG_PRETTY_NAME="Wine ($PKG_VER)"
+PKG_NEED_AUTORECONF=true
 
 BLACKLIST_ARCH=aarch64
 
-GIT_URL=https://github.com/KreitinnSoftware/wine
-GIT_COMMIT=8966a2e4ba20027aa956bdd08296543f4c7acafc
-HOST_BUILD_CONFIGURE_ARGS="--enable-win64 --without-x"
+GIT_URL="https://gitlab.winehq.org/wine/wine.git"
+GIT_COMMIT="refs/tags/wine-10.8"
+
+# Caminho para a pasta onde o wine-tools será compilado (necessário para WoW64)
 HOST_BUILD_FOLDER="$INIT_DIR/workdir/$package/wine-tools"
-HOST_BUILD_MAKE="make -j $(nproc) __tooldeps__ nls/all"
+HOST_BUILD_CONFIGURE_ARGS="--enable-win64 --without-x"
+HOST_BUILD_MAKE="make -j$(nproc) __tooldeps__ nls/all"
+
+# Caminho onde o Wine será instalado
 OVERRIDE_PREFIX="$(realpath $PREFIX/../wine)"
+
 CONFIGURE_ARGS="--enable-archs=i386,x86_64 \
-				--host=$TOOLCHAIN_TRIPLE \
-				--with-wine-tools=$INIT_DIR/workdir/$package/wine-tools \
-				--prefix=$OVERRIDE_PREFIX \
-				--without-oss \
-				--disable-winemenubuilder \
-				--disable-win16 \
-				--disable-tests \
-				--with-x \
-				--x-libraries=$PREFIX/lib \
-				--x-includes=$PREFIX/include \
-				--with-pulse \
-				--with-gstreamer \
-				--with-opengl \
-				--with-gnutls \
-				--with-mingw=gcc \
-				--with-xinput \
-				--with-xinput2 \
-    				--enable-nls \
-				--without-xshm \
-				--without-xxf86vm \
-				--without-osmesa \
-				--without-usb \
-				--without-sdl \
-				--without-cups \
-				--without-netapi \
-				--without-pcap \
-				--without-gphoto \
-				--without-v4l2 \
-				--without-pcsclite \
-				--without-wayland \
-				--without-opencl \
-				--without-dbus \
-				--without-sane \
-				--without-udev \
-				--without-capi"
+                --host=$TOOLCHAIN_TRIPLE \
+                --with-wine-tools=$HOST_BUILD_FOLDER \
+                --prefix=$OVERRIDE_PREFIX \
+                --without-oss \
+                --disable-winemenubuilder \
+                --disable-win16 \
+                --disable-tests \
+                --with-x \
+                --x-libraries=$PREFIX/lib \
+                --x-includes=$PREFIX/include \
+                --with-pulse \
+                --with-gstreamer \
+                --with-opengl \
+                --with-gnutls \
+                --with-mingw=gcc \
+                --with-xinput \
+                --with-xinput2 \
+                --enable-nls \
+                --without-xshm \
+                --without-xxf86vm \
+                --without-osmesa \
+                --without-usb \
+                --without-sdl \
+                --without-cups \
+                --without-netapi \
+                --without-pcap \
+                --without-gphoto \
+                --without-v4l2 \
+                --without-pcsclite \
+                --without-wayland \
+                --without-opencl \
+                --without-dbus \
+                --without-sane \
+                --without-udev \
+                --without-capi"
