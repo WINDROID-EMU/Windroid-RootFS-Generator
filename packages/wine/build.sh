a679ba0b4bb9d1d2f1e27489785467fb805f454b
WINDROID-EMU <45230455+WINDROID-EMU@users.noreply.github.com>
2025-06-30 15:23:47 -0300

    wine esync teste

--- packages/wine/build.sh
+++ packages/wine/build.sh
@@ -4,8 +4,9 @@ PKG_PRETTY_NAME="Wine ($PKG_VER)"
 
 BLACKLIST_ARCH=aarch64
 
-GIT_URL=https://github.com/KreitinnSoftware/wine
-GIT_COMMIT=1a4492046555dd13ddee5d7f12fa29f9ed47b697
+GIT_URL=https://github.com/WINDROID-EMU/Windroid-Wine.git
+GIT_COMMIT=dev
+
 HOST_BUILD_CONFIGURE_ARGS="--enable-win64 --without-x"
 HOST_BUILD_FOLDER="$INIT_DIR/workdir/$package/wine-tools"
 HOST_BUILD_MAKE="make -j $(nproc) __tooldeps__ nls/all"
@@ -28,7 +29,7 @@ CONFIGURE_ARGS="--enable-archs=i386,x86_64 \
 				--with-mingw=gcc \
 				--with-xinput \
 				--with-xinput2 \
-    				--enable-nls \
+				--enable-nls \
 				--without-xshm \
 				--without-xxf86vm \
 				--without-osmesa \
