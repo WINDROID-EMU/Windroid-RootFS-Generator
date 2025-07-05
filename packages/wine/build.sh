828afc1c4e9af22c415d1500ce1b3a066f7ba6df
WINDROID-EMU <45230455+WINDROID-EMU@users.noreply.github.com>
2025-07-05 19:53:08 -0300

    Proton Novamente

--- packages/wine/build.sh
+++ packages/wine/build.sh
@@ -5,7 +5,7 @@ PKG_PRETTY_NAME="Wine ($PKG_VER)"
 BLACKLIST_ARCH=aarch64
 
 GIT_URL=https://github.com/WINDROID-EMU/Windroid-Proton
-GIT_COMMIT=f653314606770b3c9e23ead9216e56f8989d9ee6
+GIT_COMMIT=84a2ceb7fde1ac7799966baddf63c248dedd9e25
 HOST_BUILD_CONFIGURE_ARGS="--enable-win64 --without-x"
 HOST_BUILD_FOLDER="$INIT_DIR/workdir/$package/wine-tools"
 HOST_BUILD_MAKE="make -j $(nproc) __tooldeps__ nls/all"
